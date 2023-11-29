249249fcd34890d8f031b5ed9282fa9bda2483cc
billups1 <eglesv2@hanmail.net>
2023-11-29 21:58:59 +0900

    첫번째 커밋

---
+++ src/main/java/hs/lessonReserve/LessonReserveApplication.java
@@ -0,0 +1,13 @@
+package hs.lessonReserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonReserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hs/lessonReserve/LessonReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package hs.lessonReserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
