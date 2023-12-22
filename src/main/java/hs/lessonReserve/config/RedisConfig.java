00602916ad4aabf69d2b742c1b34b7a48efc645b
billups1 <eglesv2@hanmail.net>
2023-12-22 16:51:16 +0900

    Github Action 배포

--- src/main/java/hs/lessonReserve/config/RedisConfig.java
+++ src/main/java/hs/lessonReserve/config/RedisConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -14,6 +15,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @EnableRedisRepositories
 @RequiredArgsConstructor
+@Profile("dev")
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
