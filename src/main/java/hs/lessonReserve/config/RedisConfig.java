ae5f47acef8caa719abba41e4bfd225c1389184a
billups1 <eglesv2@hanmail.net>
2023-12-22 20:52:23 +0900

    Github Action 배포 v7 (레디스 연결 시도)

--- src/main/java/hs/lessonReserve/config/RedisConfig.java
+++ src/main/java/hs/lessonReserve/config/RedisConfig.java
@@ -15,7 +15,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @EnableRedisRepositories
 @RequiredArgsConstructor
-@Profile("dev")
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
--- src/test/java/hs/lessonReserve/LessonReserveApplicationTests.java
+++ src/test/java/hs/lessonReserve/LessonReserveApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LessonReserveApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
